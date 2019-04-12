54e69b173bc41690bf9abbf764eb3271cf24c0f4
Kent <le6790@gmail.com>
2019-04-12 01:33:22 -0600

    Finished Vader Rule based Sentiment

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import sys
 import os
 import csv
+import re
 from nltk import word_tokenize 
 #import the graph that I forgot the name of
+import matplotlib
 import textblob 
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
@@ -10,12 +12,31 @@ from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 newSentiment = {"madness": 0}
 
 vader = SentimentIntensityAnalyzer()
-vader.lexicon.update(newSentiment)
+#vader.lexicon.update(newSentiment)
 
+
+
+def vaderSentimentAnalysis(csvData, filename):
+
+    csvFile = "{}_vader.csv".format(filename)
+    with open(csvFile, 'w') as fOut:
+        csvWriter = csv.writer(fOut, delimiter=",", lineterminator='\n', quotechar='"')
+        for i in (csvData):
+            tweetList = csvData[i]
+            totalSentVal = 0
+            for tweet in tweetList:
+                vader_dict = vader.polarity_scores(tweet)
+                totalSentVal = totalSentVal + float(vader_dict['compound'])
+            totalSentVal = (totalSentVal/len(tweetList))*10
+
+            csvWriter.writerow([i,totalSentVal])
 
 
 
 #Clean up the tweets
+def clean_tweet(tweet):
+    return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t]) |(\w+:\/\/\S+)", " ", tweet).split()) 
+
 
 
 
@@ -35,11 +56,13 @@ def main():
 
 
         for row in csvReader:
-            csvData[row[0][0:10]].append(row[3])
+            csvData[row[0][0:10]].append(clean_tweet(row[3]))
             
 
-        print(len(csvData["2019-03-01"]))
+        # print(len(csvData["2019-03-01"]))
         print(csvData["2019-03-01"])
+
+        print(vaderSentimentAnalysis(csvData, filename[:-4]))
             
 
 
